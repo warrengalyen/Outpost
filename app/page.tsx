f69a4e089bf896edfc791f8a22f8544b57340185
Warren Galyen <wgalyen@hotmail.com>
2023-08-10 09:26:56 -0700

    add component primitives

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [dark, setDark] = useState  (false)
   return (
-      <main className="">
-        <Button primary={true} size="md" label="dd"></Button>
-        <h1>Hello</h1>
-      </main>
-  )
+    <>
+      <Button variant="default" size="default">
+        Shop now!
+      </Button>
+      <h1>Hello</h1>
+    </>
+  );
 }
