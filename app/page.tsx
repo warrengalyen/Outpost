302e3c2093e8d43bc2b7828b87a19b35f9c1aca7
Warren Galyen <wgalyen@hotmail.com>
2023-08-10 09:11:38 -0700

    init storybook

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [dark, setDark] = useState  (false)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
