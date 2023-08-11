5663234b295365378c5f02b1949d48caa36f1e61
Warren Galyen <wgalyen@hotmail.com>
2023-08-11 10:47:08 -0700

    add products and stores schema

--- db/db.ts
+++ db/db.ts
@@ -2,6 +2,8 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import { migrate } from "drizzle-orm/mysql2/migrator";
+import { Simulate } from "react-dom/test-utils";
+import drop = Simulate.drop;
 
 // create the connection
 const connection = connect({
@@ -11,4 +13,6 @@ const connection = connect({
 });
 
 export const db = drizzle(connection);
+
+// syncs the migrations folder to PlanetScale
 migrate(db as any, { migrationsFolder: "./migrations-folder" });
--- migrations-folder/0000_third_hemingway.sql
+++ migrations-folder/0000_third_hemingway.sql
@@ -1,5 +1,4 @@
 CREATE TABLE `stores` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`store_name` text,
-	CONSTRAINT `stores_id` PRIMARY KEY(`id`)
+	`id` serial AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	`store_name` text
 );
