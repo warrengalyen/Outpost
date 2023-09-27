46b49579151850f0355f5b365705aabce7406565
warrengalyen <wgalyen@hotmail.com>
2023-09-27 09:58:47 -0700

    wip: migrate to server actions for products

--- app/account/selling/products/@modal/(..)product/new/page.tsx
+++ app/account/selling/products/@modal/(..)product/new/page.tsx
@@ -3,12 +3,25 @@
 import { ProductEditor } from "@/components/admin/product-editor";
 import { AlertDialogContent } from "@/components/ui/alert-dialog";
 import { AlertDialog } from "@radix-ui/react-alert-dialog";
+import {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "@/server-actions/products";
 
 export default function NewProductModal() {
   return (
     <AlertDialog defaultOpen>
       <AlertDialogContent>
-        <ProductEditor displayType="modal" productStatus="new-product" />
+        <ProductEditor
+          displayType="modal"
+          productStatus="new-product"
+          productActions={{
+            createProduct,
+            updateProduct,
+            deleteProduct,
+          }}
+        />
       </AlertDialogContent>
     </AlertDialog>
   );
