4edfcaf2f050c21c29d930de73f15ca35434c928
warrengalyen <wgalyen@hotmail.com>
2023-09-28 08:19:59 -0700

    complete refactor of product actions

--- app/account/selling/products/@modal/(..)product/new/page.tsx
+++ app/account/selling/products/@modal/(..)product/new/page.tsx
@@ -3,25 +3,12 @@
 import { ProductEditor } from "@/components/admin/product-editor";
 import { AlertDialogContent } from "@/components/ui/alert-dialog";
 import { AlertDialog } from "@radix-ui/react-alert-dialog";
-import {
-  createProduct,
-  deleteProduct,
-  updateProduct,
-} from "@/server-actions/products";
 
 export default function NewProductModal() {
   return (
     <AlertDialog defaultOpen>
       <AlertDialogContent>
-        <ProductEditor
-          displayType="modal"
-          productStatus="new-product"
-          productActions={{
-            createProduct,
-            updateProduct,
-            deleteProduct,
-          }}
-        />
+        <ProductEditor displayType="modal" productStatus="new-product" />
       </AlertDialogContent>
     </AlertDialog>
   );
